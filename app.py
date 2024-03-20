f39e331611c4c3317cc694f345952f7c1fdef233
HyeokJoon <fufu98@naver.com>
2024-03-20 10:32:00 +0900

    [docs]마이페이지 작성 시작

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ from bs4 import BeautifulSoup
 
 import json
 import sys
-import hashlib, datetime, jwt
+import hashlib
+import datetime
+import jwt
 
 app = Flask(__name__)
 
@@ -20,24 +22,30 @@ db = client.gonggu
 
 SECRET_KEY = 'SECRETT'
 
+
 @app.route('/api/signup', methods=['POST'])
 def api_register():
     id_receive = request.form['uid']
     pw_receive = request.form['pw']
     name_receive = request.form['name']
     phone_receive = request.form['phoneNum']
-    print('id: '+id_receive+' pw: '+pw_receive+' name: '+name_receive+' phone: '+phone_receive)
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest() #해시 함수 sha256(단방향) 사용해 암호화
-    
-    result = db.users.insert_one({'uid': id_receive, 'pw': pw_hash, 'uname': name_receive, 'phoneNum': phone_receive})
+    print('id: '+id_receive+' pw: '+pw_receive +
+          ' name: '+name_receive+' phone: '+phone_receive)
+    pw_hash = hashlib.sha256(pw_receive.encode(
+        'utf-8')).hexdigest()  # 해시 함수 sha256(단방향) 사용해 암호화
+
+    result = db.users.insert_one(
+        {'uid': id_receive, 'pw': pw_hash, 'uname': name_receive, 'phoneNum': phone_receive})
 
     return jsonify({'result': 'success'})
+
 
 @app.route('/api/login', methods=['POST'])
 def api_login():
     id_input = request.form['uid']
     pw_input = request.form['pw']
-    pw_hash = hashlib.sha256(pw_input.encode('utf-8')).hexdigest() #해시 함수 sha256(단방향) 사용해 암호화
+    pw_hash = hashlib.sha256(pw_input.encode(
+        'utf-8')).hexdigest()  # 해시 함수 sha256(단방향) 사용해 암호화
     result = db.users.find_one({'uid': id_input, 'pw': pw_hash})
 
     print(result)
@@ -58,21 +66,26 @@ def api_login():
 def home():
     return render_template('logIn.html')
 
+
 @app.route('/toMain')
 def toMain():
     return render_template('main.html')
+
 
 @app.route('/toLogin')
 def toLogin():
     return render_template('logIn.html')
 
+
 @app.route('/toSignUp')
 def toSignUp():
     return render_template('signUp.html')
 
+
 if __name__ == '__main__':
     print(sys.executable)
     app.run('0.0.0.0', port=5001, debug=True)
+
 
 @app.route('api/add/product', methods=['POST'])
 def insert_prod():
@@ -81,7 +94,8 @@ def insert_prod():
     minNum = request.form['minNum']
     sid = request.form['uid_give']
 
-    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36', "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3"}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+               "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3"}
     response = requests.get(url_receive, headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
     pname = soup.select_one(".prod-buy-header__title").text.strip()
@@ -99,105 +113,113 @@ def insert_prod():
 
     now = datetime.datetime.now()
     date = now + datetime.timedelta(days=7)
-    result = db.products.insert_one({'url':url_receive, 'price':price,'imgurl':img,'pname':pname, 'minNum':minNum, 'state':'모집 중', 'sid':sid, 'date':date})
+    result = db.products.insert_one({'url': url_receive, 'price': price, 'imgurl': img,
+                                    'pname': pname, 'minNum': minNum, 'state': '모집 중', 'sid': sid, 'date': date})
     pid = result.inserted_id
-    if db.products.find_one({'_id' : pid}) :
-        return jsonify({'result':'success'})
+    if db.products.find_one({'_id': pid}):
+        return jsonify({'result': 'success'})
     else:
-        return jsonify({'result':'failure'})
+        return jsonify({'result': 'failure'})
 
-        
+
 @app.route('/api/prod/ing/show', methods=['POST'])
 def show():
     uid = request.form['uid_give']
-    prod_list = list(db.products.find({'sid':'uid'}))
+    prod_list = list(db.products.find({'sid': 'uid'}))
     if prod_list:
-        return jsonify({'result':'success','products_list':prod_list})
+        return jsonify({'result': 'success', 'products_list': prod_list})
     else:
-        return jsonify({'result':'failure'})
+        return jsonify({'result': 'failure'})
+
 
 @app.route('/api/prod/in/show', methods=['POST'])
 def show():
     uid = request.form['uid_give']
-    pidlist = list(db.party.find({'uid':uid}))
+    pidlist = list(db.party.find({'uid': uid}))
     prod_list = []
     for pid in pidlist:
-        prod = db.products.find({'_id':pid})
+        prod = db.products.find({'_id': pid})
         prod_list.append(prod)
-    
+
     if prod_list:
-        return jsonify({'result':'success', 'products_list':prod_list})
+        return jsonify({'result': 'success', 'products_list': prod_list})
     else:
-        return jsonify({'result':'failure'})
+        return jsonify({'result': 'failure'})
+
 
 @app.route('/api/history/show', methods=['POST'])
 def show():
     uid = request.form['uid_give']
-    his_list = list(db.history.find({'uid':uid}))
+    his_list = list(db.history.find({'uid': uid}))
     if his_list:
-        return jsonify({'result':'success', 'products_list':his_list})
+        return jsonify({'result': 'success', 'products_list': his_list})
     else:
-        return jsonify({'result':'failure'})
+        return jsonify({'result': 'failure'})
+
 
 @app.route('/api/check/Duplicate', methods=['POST'])
 def check():
     id_give = request.form['inputId']
-    user = db.users.find({'uid':id_give})
+    user = db.users.find({'uid': id_give})
     if user:
         return jsonify({'result': 'success'})
     else:
         return jsonify({'result': 'failure'})
 
+
 @app.route('api/complete', methods=['POST'])
 def complete():
     pid = request.form['pid']
-    product = db.products.find_one({'_id':pid})
+    product = db.products.find_one({'_id': pid})
     price = product['price']
     imgurl = product['img']
     pname = product['pname']
     uid = product['sid']
     date = datetime.datetime.now()
-    phoneNum = db.users.find_one({'uid':uid})['phoneNum']
-    
-    res1 = db.history.insert_one({'pid':pid,'price':price,'imgurl':imgurl, 'pname':pname, 'date':date, 'uid':uid, 'phoneNum':phoneNum})
+    phoneNum = db.users.find_one({'uid': uid})['phoneNum']
+
+    res1 = db.history.insert_one({'pid': pid, 'price': price, 'imgurl': imgurl,
+                                 'pname': pname, 'date': date, 'uid': uid, 'phoneNum': phoneNum})
     ID = res1.inserted_id
-    res2 = db.products.delete_one({'pid':pid})
+    res2 = db.products.delete_one({'pid': pid})
     delCount = res2.deleted_count
-    if db.products.find_one({'_id' : ID}) and delCount :
-        return jsonify({'result':'success'})
+    if db.products.find_one({'_id': ID}) and delCount:
+        return jsonify({'result': 'success'})
     else:
-        return jsonify({'result':'failure'})
-    
+        return jsonify({'result': 'failure'})
+
+
 @app.route('api/party', methods=['POST'])
 def participate():
     pid = request.form['pid']
     uid = request.form['uid']
 
-    result = db.party.insert_one({'pid':pid, 'uid':uid})
+    result = db.party.insert_one({'pid': pid, 'uid': uid})
     ID = result.inserted_id
-    if db.party.find_one({'_id':ID}):
-        return jsonify({'result':'success'})
+    if db.party.find_one({'_id': ID}):
+        return jsonify({'result': 'success'})
     else:
-        return jsonify({'result':'failure'})
-    
+        return jsonify({'result': 'failure'})
+
+
 @app.route('api/party/cancel', methods=['POST'])
 def cancel():
     pid = request.form['pid']
     uid = request.form['uid']
 
-    result = db.party.delete_one({'pid':pid, 'uid':uid})
+    result = db.party.delete_one({'pid': pid, 'uid': uid})
     if result.deleted_count:
-        return jsonify({'result':'success'})
+        return jsonify({'result': 'success'})
     else:
-        return jsonify({'result':'failure'})
-    
+        return jsonify({'result': 'failure'})
+
+
 @app.route('api/buy/', methods=['POST'])
 def buy():
     pid = request.form['pid']
-    result = db.products.update_one({'_id':pid}, {'$set':{'state':'배송중'}})
+    result = db.products.update_one({'_id': pid}, {'$set': {'state': '배송중'}})
 
     if result.acknowledged:
-        return jsonify({'result':'success'})
+        return jsonify({'result': 'success'})
     else:
-        return jsonify({'result':'failure'})
-    
+        return jsonify({'result': 'failure'})
